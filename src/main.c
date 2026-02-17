4215c526924ab8817af82dcc7202f819ee60b367
Karol Wojslaw <karol.wojslaw@tieto.com>
2026-02-17 02:55:02 +0100

    Replace CoAP get with post, add integration with ThingsBoard and add random temperature generation

--- src/main.c
+++ src/main.c
@@ -10,34 +10,36 @@
 #if defined(CONFIG_POSIX_API)
 #include <zephyr/posix/arpa/inet.h>
 #include <zephyr/posix/netdb.h>
-#include <zephyr/posix/sys/socket.h>
 #include <zephyr/posix/poll.h>
+#include <zephyr/posix/sys/socket.h>
 #else
 #include <zephyr/net/socket.h>
 #endif /* CONFIG_POSIX_API */
 
 #include <zephyr/kernel.h>
-#include <zephyr/sys/reboot.h>
-#include <zephyr/net/coap.h>
-#include <zephyr/net/socket.h>
-#include <zephyr/net/conn_mgr_connectivity.h>
-#include <zephyr/net/conn_mgr_monitor.h>
-#include <zephyr/random/random.h>
-#include <zephyr/net/coap_client.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/logging/log_ctrl.h>
+#include <zephyr/net/coap.h>
+#include <zephyr/net/coap_client.h>
+#include <zephyr/net/conn_mgr_connectivity.h>
+#include <zephyr/net/conn_mgr_monitor.h>
+#include <zephyr/net/socket.h>
+#include <zephyr/random/random.h>
+#include <zephyr/sys/reboot.h>
 
 LOG_MODULE_REGISTER(coap_client_sample, CONFIG_COAP_CLIENT_SAMPLE_LOG_LEVEL);
+
+#define MAX_TELEMETRY_SIZE_BYTES 128
 
 /* Macros used to subscribe to specific Zephyr NET management events. */
 #define L4_EVENT_MASK (NET_EVENT_L4_CONNECTED | NET_EVENT_L4_DISCONNECTED)
 #define CONN_LAYER_EVENT_MASK (NET_EVENT_CONN_IF_FATAL_ERROR)
 
 /* Macro called upon a fatal error, reboots the device. */
-#define FATAL_ERROR()					\
-	LOG_ERR("Fatal error! Rebooting the device.");	\
-	LOG_PANIC();					\
-	IF_ENABLED(CONFIG_REBOOT, (sys_reboot(0)))
+#define FATAL_ERROR()                            \
+  LOG_ERR("Fatal error! Rebooting the device."); \
+  LOG_PANIC();                                   \
+  IF_ENABLED(CONFIG_REBOOT, (sys_reboot(0)))
 
 /* Zephyr NET management event callback structures. */
 static struct net_mgmt_event_callback l4_cb;
@@ -50,202 +52,204 @@ static bool is_connected;
 K_MUTEX_DEFINE(network_connected_lock);
 K_CONDVAR_DEFINE(network_connected);
 
-static int server_resolve(struct sockaddr_storage *server)
-{
-	int err;
-	struct addrinfo *result;
-	struct addrinfo hints = {
-		.ai_family = AF_INET,
-		.ai_socktype = SOCK_DGRAM
-	};
-	char ipv4_addr[NET_IPV4_ADDR_LEN];
+static int server_resolve(struct sockaddr_storage* server) {
+  int err;
+  struct addrinfo* result;
+  struct addrinfo hints = {.ai_family = AF_INET, .ai_socktype = SOCK_DGRAM};
+  char ipv4_addr[NET_IPV4_ADDR_LEN];
 
-	err = getaddrinfo(CONFIG_COAP_SAMPLE_SERVER_HOSTNAME, NULL, &hints, &result);
-	if (err) {
-		LOG_ERR("getaddrinfo, error: %d", err);
-		return err;
-	}
+  err = getaddrinfo(CONFIG_COAP_SAMPLE_SERVER_HOSTNAME, NULL, &hints, &result);
+  if (err) {
+    LOG_ERR("getaddrinfo, error: %d", err);
+    return err;
+  }
 
-	if (result == NULL) {
-		LOG_ERR("Address not found");
-		return -ENOENT;
-	}
+  if (result == NULL) {
+    LOG_ERR("Address not found");
+    return -ENOENT;
+  }
 
-	/* IPv4 Address. */
-	struct sockaddr_in *server4 = ((struct sockaddr_in *)server);
+  /* IPv4 Address. */
+  struct sockaddr_in* server4 = ((struct sockaddr_in*)server);
 
-	server4->sin_addr.s_addr = ((struct sockaddr_in *)result->ai_addr)->sin_addr.s_addr;
-	server4->sin_family = AF_INET;
-	server4->sin_port = htons(CONFIG_COAP_SAMPLE_SERVER_PORT);
+  server4->sin_addr.s_addr = ((struct sockaddr_in*)result->ai_addr)->sin_addr.s_addr;
+  server4->sin_family = AF_INET;
+  server4->sin_port = htons(CONFIG_COAP_SAMPLE_SERVER_PORT);
 
-	inet_ntop(AF_INET, &server4->sin_addr.s_addr, ipv4_addr, sizeof(ipv4_addr));
+  inet_ntop(AF_INET, &server4->sin_addr.s_addr, ipv4_addr, sizeof(ipv4_addr));
 
-	LOG_INF("IPv4 Address found %s", ipv4_addr);
+  LOG_INF("IPv4 Address found %s", ipv4_addr);
 
-	/* Free the address. */
-	freeaddrinfo(result);
+  /* Free the address. */
+  freeaddrinfo(result);
 
-	return 0;
+  return 0;
 }
 
-static void wait_for_network(void)
-{
-	k_mutex_lock(&network_connected_lock, K_FOREVER);
+static void wait_for_network(void) {
+  k_mutex_lock(&network_connected_lock, K_FOREVER);
 
-	if (!is_connected) {
-		LOG_INF("Waiting for network connectivity");
-		k_condvar_wait(&network_connected, &network_connected_lock, K_FOREVER);
-	}
+  if (!is_connected) {
+    LOG_INF("Waiting for network connectivity");
+    k_condvar_wait(&network_connected, &network_connected_lock, K_FOREVER);
+  }
 
-	k_mutex_unlock(&network_connected_lock);
+  k_mutex_unlock(&network_connected_lock);
 }
 
-static void response_cb(int16_t code, size_t offset, const uint8_t *payload,
-			size_t len, bool last_block, void *user_data)
-{
-	if (code >= 0) {
-		LOG_INF("CoAP response: code: 0x%x, payload: %s", code, payload);
-	} else {
-		LOG_INF("Response received with error code: %d", code);
-	}
+static void response_cb(int16_t code, size_t offset, const uint8_t* payload, size_t len, bool last_block,
+                        void* user_data) {
+  if (code >= 0) {
+    LOG_INF("CoAP response: code: 0x%x, payload: %s", code, (char*)(payload ? (char*)payload : "NULL"));
+  } else {
+    LOG_INF("Response received with error code: %d", code);
+  }
 }
 
-static int periodic_coap_request_loop(void)
-{
-	int err, sock;
-	struct sockaddr_storage server = { 0 };
-	struct coap_client coap_client = { 0 };
-	struct coap_client_request req = {
-		.method = COAP_METHOD_GET,
-		.confirmable = true,
-		.fmt = COAP_CONTENT_FORMAT_TEXT_PLAIN,
-		.payload = NULL,
-		.cb = response_cb,
-		.len = 0,
-		.path = CONFIG_COAP_SAMPLE_RESOURCE,
-	};
+static int periodic_coap_request_loop(void) {
+  int err, sock;
+  struct sockaddr_storage server = {0};
+  struct coap_client coap_client = {0};
+  struct coap_client_request req = {
+      .method = COAP_METHOD_POST,
+      .confirmable = true,
+      .fmt = COAP_CONTENT_FORMAT_APP_JSON,
+      .cb = response_cb,
+      .path = CONFIG_COAP_SAMPLE_RESOURCE,
+  };
 
-	err = server_resolve(&server);
-	if (err) {
-		LOG_ERR("Failed to resolve server name");
-		return err;
-	}
+  err = server_resolve(&server);
+  if (err) {
+    LOG_ERR("Failed to resolve server name");
+    return err;
+  }
 
-	sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-	if (sock < 0) {
-		LOG_ERR("Failed to create CoAP socket: %d.", -errno);
-		return -errno;
-	}
+  sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+  if (sock < 0) {
+    LOG_ERR("Failed to create CoAP socket: %d.", -errno);
+    return -errno;
+  }
 
-	LOG_INF("Initializing CoAP client");
+  LOG_INF("Initializing CoAP client");
 
-	err = coap_client_init(&coap_client, NULL);
-	if (err) {
-		LOG_ERR("Failed to initialize CoAP client: %d", err);
-		return err;
-	}
+  err = coap_client_init(&coap_client, NULL);
+  if (err) {
+    LOG_ERR("Failed to initialize CoAP client: %d", err);
+    return err;
+  }
 
-	while (true) {
-		wait_for_network();
+  while (true) {
+    wait_for_network();
 
-		/* Send request */
-		err = coap_client_req(&coap_client, sock, (struct sockaddr *)&server, &req, NULL);
-		if (err) {
-			LOG_ERR("Failed to send request: %d", err);
-			return err;
-		}
+    // Generate random temperatures
+    uint32_t t1 = (sys_rand32_get() % 10) + 15;
+    uint32_t t2 = (sys_rand32_get() % 5) + 25;
 
-		LOG_INF("CoAP GET request sent sent to %s, resource: %s",
-			CONFIG_COAP_SAMPLE_SERVER_HOSTNAME, CONFIG_COAP_SAMPLE_RESOURCE);
+    char temp_temperature_str[10] = "";
 
-		k_sleep(K_SECONDS(CONFIG_COAP_SAMPLE_REQUEST_INTERVAL_SECONDS));
-	}
+    char payload[MAX_TELEMETRY_SIZE_BYTES + 1] = "";  // "{\"t1\":\"1.15\", \"t2\":\"2.24\"}"
+    strncat(payload, "{\"t1\":\"", MAX_TELEMETRY_SIZE_BYTES - strlen(payload));
+    snprintf(temp_temperature_str, 10, "%u", t1);
+    strncat(payload, temp_temperature_str, MAX_TELEMETRY_SIZE_BYTES - strlen(payload));
+    strncat(payload, "\", \"t2\":\"", MAX_TELEMETRY_SIZE_BYTES - strlen(payload));
+    snprintf(temp_temperature_str, 10, "%u", t2);
+    strncat(payload, temp_temperature_str, MAX_TELEMETRY_SIZE_BYTES - strlen(payload));
+    strncat(payload, "\"}", MAX_TELEMETRY_SIZE_BYTES - strlen(payload));
+
+    req.payload = payload;
+    req.len = strlen((char*)payload);
+
+    /* Send request */
+    err = coap_client_req(&coap_client, sock, (struct sockaddr*)&server, &req, NULL);
+    if (err) {
+      LOG_ERR("Failed to send request: %d", err);
+      return err;
+    }
+
+    LOG_INF("CoAP POST request sent to %s, resource: %s", CONFIG_COAP_SAMPLE_SERVER_HOSTNAME,
+            CONFIG_COAP_SAMPLE_RESOURCE);
+
+    k_sleep(K_SECONDS(CONFIG_COAP_SAMPLE_REQUEST_INTERVAL_SECONDS));
+  }
 }
 
-static void l4_event_handler(struct net_mgmt_event_callback *cb,
-			     uint64_t event,
-			     struct net_if *iface)
-{
-	switch (event) {
-	case NET_EVENT_L4_CONNECTED:
-		LOG_INF("Network connectivity established");
-		k_mutex_lock(&network_connected_lock, K_FOREVER);
-		is_connected = true;
-		k_condvar_signal(&network_connected);
-		k_mutex_unlock(&network_connected_lock);
-		break;
-	case NET_EVENT_L4_DISCONNECTED:
-		LOG_INF("Network connectivity lost");
-		k_mutex_lock(&network_connected_lock, K_FOREVER);
-		is_connected = false;
-		k_mutex_unlock(&network_connected_lock);
-		break;
-	default:
-		/* Don't care */
-		return;
-	}
+static void l4_event_handler(struct net_mgmt_event_callback* cb, uint64_t event, struct net_if* iface) {
+  switch (event) {
+    case NET_EVENT_L4_CONNECTED:
+      LOG_INF("Network connectivity established");
+      k_mutex_lock(&network_connected_lock, K_FOREVER);
+      is_connected = true;
+      k_condvar_signal(&network_connected);
+      k_mutex_unlock(&network_connected_lock);
+      break;
+    case NET_EVENT_L4_DISCONNECTED:
+      LOG_INF("Network connectivity lost");
+      k_mutex_lock(&network_connected_lock, K_FOREVER);
+      is_connected = false;
+      k_mutex_unlock(&network_connected_lock);
+      break;
+    default:
+      /* Don't care */
+      return;
+  }
 }
-static void connectivity_event_handler(struct net_mgmt_event_callback *cb,
-						uint64_t event,
-						struct net_if *iface)
-{
-	if (event == NET_EVENT_CONN_IF_FATAL_ERROR) {
-		LOG_ERR("NET_EVENT_CONN_IF_FATAL_ERROR");
-		FATAL_ERROR();
-		return;
-	}
+static void connectivity_event_handler(struct net_mgmt_event_callback* cb, uint64_t event, struct net_if* iface) {
+  if (event == NET_EVENT_CONN_IF_FATAL_ERROR) {
+    LOG_ERR("NET_EVENT_CONN_IF_FATAL_ERROR");
+    FATAL_ERROR();
+    return;
+  }
 }
 
-int main(void)
-{
-	int err;
+int main(void) {
+  int err;
 
-	LOG_INF("The CoAP client sample started");
+  LOG_INF("The CoAP client sample started");
 
-	/* Setup handler for Zephyr NET Connection Manager events and Connectivity layer. */
-	net_mgmt_init_event_callback(&l4_cb, l4_event_handler, L4_EVENT_MASK);
-	net_mgmt_add_event_callback(&l4_cb);
+  /* Setup handler for Zephyr NET Connection Manager events and Connectivity layer. */
+  net_mgmt_init_event_callback(&l4_cb, l4_event_handler, L4_EVENT_MASK);
+  net_mgmt_add_event_callback(&l4_cb);
 
-	net_mgmt_init_event_callback(&conn_cb, connectivity_event_handler, CONN_LAYER_EVENT_MASK);
-	net_mgmt_add_event_callback(&conn_cb);
+  net_mgmt_init_event_callback(&conn_cb, connectivity_event_handler, CONN_LAYER_EVENT_MASK);
+  net_mgmt_add_event_callback(&conn_cb);
 
-	/* Bring all network interfaces up.
-	 * Wi-Fi or LTE depending on the board that the sample was built for.
-	 */
-	LOG_INF("Bringing network interface up and connecting to the network");
+  /* Bring all network interfaces up.
+   * Wi-Fi or LTE depending on the board that the sample was built for.
+   */
+  LOG_INF("Bringing network interface up and connecting to the network");
 
-	err = conn_mgr_all_if_up(true);
-	if (err) {
-		LOG_ERR("conn_mgr_all_if_up, error: %d", err);
-		FATAL_ERROR();
-		return err;
-	}
+  err = conn_mgr_all_if_up(true);
+  if (err) {
+    LOG_ERR("conn_mgr_all_if_up, error: %d", err);
+    FATAL_ERROR();
+    return err;
+  }
 
-	err = conn_mgr_all_if_connect(true);
-	if (err) {
-		LOG_ERR("conn_mgr_all_if_connect, error: %d", err);
-		FATAL_ERROR();
-		return err;
-	}
+  err = conn_mgr_all_if_connect(true);
+  if (err) {
+    LOG_ERR("conn_mgr_all_if_connect, error: %d", err);
+    FATAL_ERROR();
+    return err;
+  }
 
-	/* Resend connection status if the sample is built for NATIVE_SIM.
-	 * This is necessary because the network interface is automatically brought up
-	 * at SYS_INIT() before main() is called.
-	 * This means that NET_EVENT_L4_CONNECTED fires before the
-	 * appropriate handler l4_event_handler() is registered.
-	 */
-	if (IS_ENABLED(CONFIG_BOARD_NATIVE_SIM)) {
-		conn_mgr_mon_resend_status();
-	}
+  /* Resend connection status if the sample is built for NATIVE_SIM.
+   * This is necessary because the network interface is automatically brought up
+   * at SYS_INIT() before main() is called.
+   * This means that NET_EVENT_L4_CONNECTED fires before the
+   * appropriate handler l4_event_handler() is registered.
+   */
+  if (IS_ENABLED(CONFIG_BOARD_NATIVE_SIM)) {
+    conn_mgr_mon_resend_status();
+  }
 
-	wait_for_network();
+  wait_for_network();
 
-	err = periodic_coap_request_loop();
-	if (err) {
-		LOG_ERR("periodic_coap_request_loop, error: %d", err);
-		FATAL_ERROR();
-		return err;
-	}
+  err = periodic_coap_request_loop();
+  if (err) {
+    LOG_ERR("periodic_coap_request_loop, error: %d", err);
+    FATAL_ERROR();
+    return err;
+  }
 
-	return 0;
+  return 0;
 }
